6420cd8a92f458b63718bc652d799e70a36cd37a
Karen V <karenvo@usc.edu>
2019-02-05 23:52:20 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/\w+[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.match(/\w+un/)
 end
 
 def words_five_letters_long(text)
-
+  text.grep(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
